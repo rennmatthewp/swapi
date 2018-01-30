32fa94b3dddb7879305600d16c64fff6b51450c0
Matthew Renn <rennmatthewp@gmail.com>
2018-01-30 16:37:51 -0700

    adds button to link to Main route

--- src/components/Crawl/Crawl.js
+++ src/components/Crawl/Crawl.js
@@ -1,15 +1,19 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 // import PropTypes from 'prop-types';
 
 const Crawl = ({ title, openingCrawl }) => {  
   return (
     <div className="crawl">
-      <h3>{title}</h3>
-      <p>{openingCrawl}</p>
+      <h3>Episode Bkuuee</h3>
+      <p>yes sure</p>
+      <Link to='/main'>
+        <button>X</button>
+      </Link>
     </div>
   );
 };
-
+ 
 // Crawl.propTypes = {
 //   title: PropTypes.string,
 //   crawlText: PropTypes.arrayOf(PropTypes.string),
