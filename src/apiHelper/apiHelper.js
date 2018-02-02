b7340aef0e065f69ea845893e3ca312e960337ef
Matthew Renn <rennmatthewp@gmail.com>
2018-02-02 13:19:40 -0700

    refactor fetchAndParse

--- src/apiHelper/apiHelper.js
+++ src/apiHelper/apiHelper.js
@@ -1,19 +1,17 @@
 export const getFilm = async () => {
   const number = Math.ceil(Math.random() * 7);
-  const crawlData = await fetchAndParse(`https://swapi.co/api/films/${number}`)
+  const crawlData = await fetchAndParse(`https://swapi.co/api/films/${number}`);
   return cleanCrawlData(crawlData);
 };
 
-const fetchAndParse = async url => {
-  const response = await fetch(url);
-  const result = response.json();
-  return result;
-};
+const fetchAndParse = url => fetch(url).then(res => res.json());
 
 const cleanCrawlData = rawCrawlData => {
   const lineBreak = new RegExp(/\s{4,}/, 'g');
-  
-  const openingCrawl = rawCrawlData.opening_crawl.replace(lineBreak, '###').split('###')
+
+  const openingCrawl = rawCrawlData.opening_crawl
+    .replace(lineBreak, '###')
+    .split('###');
 
   return {
     crawlText: openingCrawl,
