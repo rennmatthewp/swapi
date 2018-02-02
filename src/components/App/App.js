22fda866211fbbcab13a8abb4a0d174ab88bc573
Matthew Renn <rennmatthewp@gmail.com>
2018-02-02 13:22:26 -0700

    changes App to Statefull component fetches crawl data in CDM passes it down to Crawl

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,20 +1,38 @@
-import React from 'react';
-import { Route, Switch } from "react-router-dom";
-import Crawl from "../Crawl/Crawl";
-import Main from "../Main/Main";
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Crawl from '../Crawl/Crawl';
+import Main from '../Main/Main';
 // import CardContainer from "../CardContainer/CardContainer";
 // import * as fetch from '../../apiHelper/apiHelper'
 import './App.css';
 
-const App = () => {
-  return (
-    <div className="App">
-      <Switch> 
-        <Route exact path='/' component={Crawl} />
-        <Route exact path='/main' component={Main} />
-      </Switch>
-    </div>
-  );
+import { getFilm } from '../../apiHelper/apiHelper';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      
+    };
+  }
+
+  async componentDidMount() {
+    const filmObj = await getFilm();
+    this.setState({ ...filmObj });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Switch>
+          <Route exact path="/" render={() => (
+            <Crawl {...this.state} /> 
+          )} />
+          <Route exact path="/main" component={Main} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
