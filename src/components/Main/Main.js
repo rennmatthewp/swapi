09d0117804c154085c2555a5dd1e32ea43dbf2da
Matthew Renn <rennmatthewp@gmail.com>
2018-02-02 13:23:40 -0700

    fetches planets data in CDM

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import CardContainer from "../CardContainer/CardContainer";
-import Header from "../Header/Header";
+import CardContainer from '../CardContainer/CardContainer';
+import Header from '../Header/Header';
 
 class Main extends Component {
   constructor() {
@@ -10,10 +10,26 @@ class Main extends Component {
       planets: [],
       vehicles: [],
       favorites: []
-    }
+    };
   }
 
-  
+  async componentDidMount() {
+    const people = await fetch('https://swapi.co/api/people/')
+      .then(response => response.json())
+      .then(result =>
+        result.results.map(person => {
+          return Promise.all([
+            fetch(person.homeworld).then(res => res.json()),
+            fetch(person.species[0]).then(res => res.json())
+          ]).then(([homeworld, species]) => Object.assign({}, person, {homeworld: homeworld.name, species: species.name}));
+        })
+      )
+
+      .catch(error => console.log('error:', error));
+    console.log('peopel:', people)
+    
+    Promise.all(people).then(res => console.log('GIVE IT ', res));
+  }
 
   render() {
     return (
