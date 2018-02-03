0df10552a1c7dec0469f0efbdf184c8f682427e5
Matthew Renn <rennmatthewp@gmail.com>
2018-02-03 13:16:44 -0700

    refactor crawlText map and capture, adds proptypes

--- src/components/Crawl/Crawl.js
+++ src/components/Crawl/Crawl.js
@@ -1,28 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 const Crawl = ({ crawlText, episode, releaseDate, title }) => {
+  const story = crawlText
+    ? crawlText.map((paragraph, index) => <p key={index}>{paragraph}</p>)
+    : null;    
 
-  console.log(crawlText)
-  
-  let story
-  if (crawlText) {
-    story = crawlText.map( paragraph => <p>{paragraph}</p> )
-  }
-  
   return (
     <Link to="/main">
       <div className="crawl">
         crwal
-        <h3>Episode {episode}</h3>
+        <h3>EPISODE {episode}</h3>
         <h3>{title}</h3>
-        {/* {crawlText &&
-          crawlText.map((paragraph, i) => <p key={i}>{paragraph}</p>)} */}
         {story}
         <h3>{releaseDate}</h3>
       </div>
     </Link>
-    );
-  }
+  );
+};
+
+Crawl.propTypes = {
+  crawlText: PropTypes.arrayOf(PropTypes.string),
+  episode: PropTypes.number,
+  releaseDate: PropTypes.string,
+  title: PropTypes.string
+};
 
 export default Crawl;
