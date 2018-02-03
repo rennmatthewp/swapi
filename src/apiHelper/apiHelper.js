db4e12ba45637a410f4e6ee325f774756474f3b5
Matthew Renn <rennmatthewp@gmail.com>
2018-02-03 13:14:05 -0700

    adds fetchPeople function

--- src/apiHelper/apiHelper.js
+++ src/apiHelper/apiHelper.js
@@ -1,4 +1,4 @@
-export const getFilm = async () => {
+export const fetchFilm = async () => {
   const number = Math.ceil(Math.random() * 7);
   const crawlData = await fetchAndParse(`https://swapi.co/api/films/${number}`);
   return cleanCrawlData(crawlData);
@@ -6,17 +6,35 @@ export const getFilm = async () => {
 
 const fetchAndParse = url => fetch(url).then(res => res.json());
 
-const cleanCrawlData = rawCrawlData => {
+const cleanCrawlData = ({ episode_id, opening_crawl, release_date, title }) => {
   const lineBreak = new RegExp(/\s{4,}/, 'g');
-
-  const openingCrawl = rawCrawlData.opening_crawl
-    .replace(lineBreak, '###')
-    .split('###');
+  const openingCrawl = opening_crawl.replace(lineBreak, '###').split('###');
 
   return {
     crawlText: openingCrawl,
-    episode: rawCrawlData.episode_id,
-    releaseDate: rawCrawlData.release_date,
-    title: rawCrawlData.title.toUpperCase()
+    episode: episode_id,
+    releaseDate: release_date,
+    title: title.toUpperCase()
   };
+};
+
+export const fetchPeople = async () => {
+  try {
+    const initialFetch = await fetchAndParse('https://swapi.co/api/people/');
+    const people = initialFetch.results.map(async person => {
+      const homeworld = await fetchAndParse(person.homeworld);
+      const species = await fetchAndParse(person.species);
+      return {
+        name: person.name,
+        homeworld: homeworld.name,
+        homeWorldPop: homeworld.population,
+        species: species.name
+      };
+    });
+  
+    return Promise.all(people);
+    
+  } catch (error) {
+    console.log('Error in fetchPeople:', error) 
+  }
 };
