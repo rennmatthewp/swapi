0063d0b2bed38a1dcb7b252aab8b9f004a0cff74
Matthew Renn <rennmatthewp@gmail.com>
2018-02-03 13:17:19 -0700

    moves fetchPeople out of Main and into apiHelper

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import CardContainer from '../CardContainer/CardContainer';
 import Header from '../Header/Header';
+import { fetchPeople } from '../../apiHelper/apiHelper';
 
 class Main extends Component {
   constructor() {
@@ -14,21 +15,8 @@ class Main extends Component {
   }
 
   async componentDidMount() {
-    const people = await fetch('https://swapi.co/api/people/')
-      .then(response => response.json())
-      .then(result =>
-        result.results.map(person => {
-          return Promise.all([
-            fetch(person.homeworld).then(res => res.json()),
-            fetch(person.species[0]).then(res => res.json())
-          ]).then(([homeworld, species]) => Object.assign({}, person, {homeworld: homeworld.name, species: species.name}));
-        })
-      )
-
-      .catch(error => console.log('error:', error));
-    console.log('peopel:', people)
-    
-    Promise.all(people).then(res => console.log('GIVE IT ', res));
+    const people = await fetchPeople();
+    this.setState({ people });
   }
 
   render() {
