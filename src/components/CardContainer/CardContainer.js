844831c373820f15e4efa2b9b696b5ab8c37e495
Matthew Renn <rennmatthewp@gmail.com>
2018-02-05 08:47:29 -0700

    gets toggleFavorites from Main and passes it into each Card

--- src/components/CardContainer/CardContainer.js
+++ src/components/CardContainer/CardContainer.js
@@ -2,9 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Card from '../Card/Card';
 
-const CardContainer = ({ cardData }) => {
+const CardContainer = ({ cardData, toggleFavorite }) => {
   const cards = cardData.map(cardObj => (
-    <Card {...cardObj} key={cardObj.name} />
+    <Card cardObj={cardObj} toggleFavorite={toggleFavorite} key={cardObj.name} />
   ));
 
   return <div>{cards}</div>;
